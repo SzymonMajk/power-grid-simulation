0bae348afa2d55c9418a5dca0177fe568813cfb0
SzymonMajk <Szymek_diks@interia.pl>
2018-01-09 00:01:13 +0100

    First alpha

--- siec.erl
+++ siec.erl
@@ -1,9 +1,10 @@
-%  siec.erl
+% siec.erl
 -module(siec).
 -compile(export_all).
 %-export([init/0,init/1,start/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DATA FORMATS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -define(timestamp, 1000).
 %powerhouse -> {id,power}
@@ -11,16 +12,15 @@
 %electricalNode -> {id,[powerhouse],[distributive]}
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    API    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 init() ->
     {prepareElectricalNodeProcess(),
     [{1,preparePowerhouseProcess()}],
     [prepareDistributiveProcess()]}.
-    %Zahardkodowana wersja
-    %TODO 1 przypadek prosty
 
-init(filename) ->
-    not_implemented.
+init(Filename) ->
+    not_implemented. %TODO
     %Czyta z pliku w formacie np. linijka po linijce elektrownie, rozdzielnie
     %i wezly sieci, tworzy z nich odpowiednie mapy, które zwraca
 
@@ -29,81 +29,96 @@ init(filename) ->
 %start(data) ->
 %start({ElectricalNode,Powerhouses,Distributives}) ->
 start({PID1,[{1,PID2}],[PID3]}) ->
-    PID1,
-    PID2 ! startPowerhouse.
+    io:fwrite("RUN!~n"),
+    PID1 ! {start,[PID2],[PID3]},
+    PID2 ! start,
+    PID3 ! start,
+    ok.
     %Otrzymuje na wejsciu krotke, gdzie jest mapa elektrowni, mapa rozdzielni i lista wezlow, do kazdego procesu wysyla wiadomosc, ze nadszedl czas, aby ten proces przelaczyl sie w tryb symulacji
 
 %Optional add stop(data)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CREATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+prepareElectricalNodeProcess() ->
+    spawn(siec,startElectricalNode,[]).
 
 preparePowerhouseProcess() ->
     spawn(siec,startPowerhouse,[]).
 
 prepareDistributiveProcess() ->
-    not_implemented.
+    spawn(siec,startDistributive,[]).
 
-prepareElectricalNodeProcess() ->
-    not_implemented.
+startElectricalNode() ->
+    receive
+        {start,Plist,Dlist}-> io:fwrite("Electrical Node started~n"),
+                runElectricalNode(Plist,Dlist,Dlist)
+    end.
 
 startPowerhouse() ->
     receive
-        startPowerhouse -> io:fwrite("ok~n")
+        start -> io:fwrite("Power House started~n"),
+                runPowerhouse(1,100,100)
     end.
 
 startDistributive() ->
-    not_implemented.
-
-startElectricalNode() ->
-    not_implemented.
+    receive
+        start -> io:fwrite("Distributive started~n"),
+                runDistributive(5,5)
+    end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SIMULATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+runElectricalNode(P,D,[DistrPID]) ->
+    DistrPID ! {frEN,self()},
+    io:fwrite("N:Asking another dist... ~n"),
+    receive
+        {frDI,Need} -> randomPowerhouse(P) ! {toPH,Need},
+        io:fwrite("N:Powerhouse choosen... ~n"),
+        timer:sleep(2000),
+        runElectricalNode(P,D,D)
+    end;
+runElectricalNode(P,D,[DistrPID,T]) ->
+    DistrPID ! {frEN,self()},
+    io:fwrite("N:Asking another dist... ~n"),
+    receive
+        {frDI,Need} -> randomPowerhouse(P) ! {toPH,Need},
+        io:fwrite("N:Powerhouse choosen... ~n")
+    end,
+    runElectricalNode(P,D,T).
 
 runPowerhouse(Id,Energy,InitialPower) ->
     receive
-    Need -> runPowerhouse(Id,Energy-Need,InitialPower)
+    {toPH,Need} -> io:fwrite("P:Use my energy!~n"),
+            runPowerhouse(Id,Energy-Need,InitialPower)
     after
-    timestamp -> 
+    1000 -> 
         saveEnergyBalance(Id,Energy),
+        io:fwrite("P:Saved Balance!~n"),
         runPowerhouse(Id,InitialPower,InitialPower)
     end.
 
 runDistributive(Need,InitialNeed) ->
     receive
-    nodePID -> 
-        nodePID ! Need,
-        runDistributive(0,InitialNeed)
+    {frEN,ENPID} -> 
+        ENPID ! {frDI,Need},
+        runDistributive(0,InitialNeed),
+        io:fwrite("D:Need sent.~n")
     after
-    timestamp -> runDistributive(InitialNeed,InitialNeed)
+    1000 -> io:fwrite("D:Need more energy!~n"),
+        runDistributive(InitialNeed,InitialNeed)
     end.
 
-%powerhouses and distr are litst of {id,PID} or lists of PIDS
-runElectricalNode(Powerhouses,Distributives,[]) ->
-    runElectricalNode(Powerhouses,Distributives,Distributives);
-runElectricalNode(Powerhouses,Distributives,[H,T]) ->
-    getDitributivePID(H) ! self(),
-    receive
-        Need -> randomPowerhouse(Powerhouses) ! Need
-    end,
-    runElectricalNode(Powerhouses,Distributives,T).
-    
-
-
-randomPowerhouse([{_,PID},_]) -> 
+randomPowerhouse([PID|_]) -> 
     PID;
 randomPowerhouse(powerhouses) ->
-    not_implemented. %TODO random
-
-getDitributivePID({_,PID}) ->
-    PID.
+    not_implemented. %TODO
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% STATISTICS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 saveEnergyBalance(FilenameId,Energy) ->
-    not_implemented.
-
-
-
-
-
+    not_implemented. %TODO
 
