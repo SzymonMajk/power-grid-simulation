941dfa9969ee5453ac9f045a0a786172ea3b72d8
SzymonMajk <Szymek_diks@interia.pl>
2018-01-10 00:02:49 +0100

    log to file added, idea for data file loaded, user interface added

--- siec.erl
+++ siec.erl
@@ -1,42 +1,69 @@
 % siec.erl
 -module(siec).
 -compile(export_all).
-%-export([init/0,init/1,start/1]).
+-export([main/0]).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DATA FORMATS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  USER INTERFACE  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--define(timestamp, 1000).
-%powerhouse -> {id,power}
-%distributive -> {id,type}
-%electricalNode -> {id,[powerhouse],[distributive]}
+main() ->
+    io:format("Type 1 to start hardcoded example~n"),
+    io:format("Type 0 to exit~n"),
+    startSimulation(input()).
+
+startSimulation(0) -> io:format("Thanks you, have a good day!~n");
+startSimulation(1) ->
+    io:format("Clear output file:~n~n"),
+    file:write_file("OutputFile",""),
+    io:format("Start prepareation:~n~n"),
+    I = initProcesses(),
+    %S = initProcesses("InputFile"), %TODO data from file reading
+    %I = createProcesses(S),
+    start(I);
+startSimulation(_) ->
+    io:format("Please follow instruction...~n"),
+    main().
+
+input()->
+    try io:fread("==simulation==>","~d") of
+        {ok, [N]} -> N
+    catch
+        _:_ -> 5 %TODO why doeas not work for string input?
+    end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    API    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-init() ->
+initProcesses() ->
     {prepareElectricalNodeProcess(),
     [{1,preparePowerhouseProcess()}],
-    [prepareDistributiveProcess()]}.
+    [prepareDistributiveProcess()]}.%Also preapare PidsList
 
-init(Filename) ->
-    not_implemented. %TODO
-    %Czyta z pliku w formacie np. linijka po linijce elektrownie, rozdzielnie
-    %i wezly sieci, tworzy z nich odpowiednie mapy, które zwraca
+initProcesses(Filename) ->
+    {ok, X} = file:consult(Filename),X. 
+    %TODO try catch for exeptions
 
-%Funkcje init mają zwrócić krotkę, w której będzie mapa z elektrowniami, mapa z rozdzielniami i lista wezlow
+testread() ->
+   file:consult("InputFile").
 
-%start(data) ->
+%start(data,PidList) -> TODO change input structure
 %start({ElectricalNode,Powerhouses,Distributives}) ->
 start({PID1,[{1,PID2}],[PID3]}) ->
     io:fwrite("RUN!~n"),
     PID1 ! {start,[PID2],[PID3]},
     PID2 ! start,
     PID3 ! start,
-    ok.
+    %startThemAll([PID1,PID2,PID3]) TODO need to thing about it
+    stop(input,[PID1,PID2,PID3]).
     %Otrzymuje na wejsciu krotke, gdzie jest mapa elektrowni, mapa rozdzielni i lista wezlow, do kazdego procesu wysyla wiadomosc, ze nadszedl czas, aby ten proces przelaczyl sie w tryb symulacji
 
-%Optional add stop(data)
+stop(0,Pids) ->
+    io:format("It is time to stop!:~n~n"),
+    stopThemAll(Pids),
+    main();
+stop(_,Pids) ->
+    io:format("Type s to stop!~n"),
+    stop(input(),Pids).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CREATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -68,6 +95,20 @@ startDistributive() ->
                 runDistributive(5,5)
     end.
 
+stopThemAll([P]) ->
+    P ! stop,
+    dupa;
+stopThemAll([H|T]) ->
+    H ! stop,
+    stopThemAll(T).
+
+startThemAll([P]) ->
+    P ! start,
+    dupa;
+startThemAll([H|T]) ->
+    H ! start,
+    stopThemAll(T).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SIMULATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -75,15 +116,17 @@ runElectricalNode(P,D,[DistrPID]) ->
     DistrPID ! {frEN,self()},
     io:fwrite("N:Asking another dist... ~n"),
     receive
+        stop -> io:fwrite("N: Stopped ~n");
         {frDI,Need} -> randomPowerhouse(P) ! {toPH,Need},
         io:fwrite("N:Powerhouse choosen... ~n"),
         timer:sleep(2000),
         runElectricalNode(P,D,D)
     end;
-runElectricalNode(P,D,[DistrPID,T]) ->
+runElectricalNode(P,D,[DistrPID|T]) ->
     DistrPID ! {frEN,self()},
     io:fwrite("N:Asking another dist... ~n"),
     receive
+        stop -> io:fwrite("N: Stopped ~n");
         {frDI,Need} -> randomPowerhouse(P) ! {toPH,Need},
         io:fwrite("N:Powerhouse choosen... ~n")
     end,
@@ -91,10 +134,11 @@ runElectricalNode(P,D,[DistrPID,T]) ->
 
 runPowerhouse(Id,Energy,InitialPower) ->
     receive
+    stop -> io:fwrite("N: Stopped ~n");
     {toPH,Need} -> io:fwrite("P:Use my energy!~n"),
             runPowerhouse(Id,Energy-Need,InitialPower)
     after
-    1000 -> 
+    1000 -> %TODO Replace time based synchronization with broadcast to them
         saveEnergyBalance(Id,Energy),
         io:fwrite("P:Saved Balance!~n"),
         runPowerhouse(Id,InitialPower,InitialPower)
@@ -102,6 +146,7 @@ runPowerhouse(Id,Energy,InitialPower) ->
 
 runDistributive(Need,InitialNeed) ->
     receive
+    stop -> io:fwrite("N: Stopped ~n");
     {frEN,ENPID} -> 
         ENPID ! {frDI,Need},
         runDistributive(0,InitialNeed),
@@ -120,5 +165,8 @@ randomPowerhouse(powerhouses) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 saveEnergyBalance(FilenameId,Energy) ->
-    not_implemented. %TODO
+    Data = integer_to_list(FilenameId) ++ " used " ++ integer_to_list(Energy),
+    LineSep = io_lib:nl(),
+    file:write_file("OutputFile",Data,[append]),
+    file:write_file("OutputFile",LineSep,[append]).
 
